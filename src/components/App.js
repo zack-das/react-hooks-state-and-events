47b162128c48081dd6a022a10406c4bbde50ddf5
zack-das <izackdas@gmail.com>
2025-10-01 09:30:38 +0300

    Updates

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+   // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  
   const [isDarkMode, setIsDarkMode] = useState(false);
   const appClass = isDarkMode ? "App dark" : "App light";
 
